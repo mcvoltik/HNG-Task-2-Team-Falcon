f4709e0754bc1c3d96fe6cfbfa88e013ad2b738b
mcvoltik <49316230+mcvoltik@users.noreply.github.com>
2020-06-03 13:00:24 +0100

    HNG-01089.py
    I assigned variables to given and then flushed the printed output.

---
+++ scripts/HNG-01089.py
@@ -0,0 +1,5 @@
+full_name = "Michael Ashi"
+id = "HNG-01089"
+language_type = "python"
+
+print(f"Hello World, this is {full_name} with HNGi7 ID {id} using {language_type} for stage 2 task", flush = True)
