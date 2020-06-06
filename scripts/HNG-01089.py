f148ad25eb8d7af6e8a84b9ba87edadeccb51571
mcvoltik <49316230+mcvoltik@users.noreply.github.com>
2020-06-06 16:25:11 +0100

    Create HNG-01089.py

---
+++ scripts/HNG-01089.py
@@ -0,0 +1,5 @@
+full_name = 'Michael Ashi'
+hngi7id = 'HNG-01089'
+language_type = 'Python'
+details = "Hello World, this is {} with HNGi7 ID {} using {} for stage 2 task. ashimichaelashi@gmail.com" .format(full_name, hngi7id, language_type)
+print(details)
